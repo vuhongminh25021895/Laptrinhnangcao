f86c9b387187b003f25662691059c5474849d89b
laptq1909 <25021843@vnu.edu.vn>
2026-04-20 08:26:54 +0700

    Add files via upload

---
+++ Launcher.java
@@ -0,0 +1,9 @@
+package com.javaproject.demo;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(PriceCompareLogic.class, args);
+    }
+}
